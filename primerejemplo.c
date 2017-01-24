5d1b1aa2a1fdd0f555253f2d5c0886914cdce077
Michael D <duartemiichael@hotmail.com>
2017-01-24 14:07:10 -0600

    Mi primer ejemplo en C.
    Determino las secciones de un proyecto.

--- primerejemplo.c
+++ primerejemplo.c
@@ -1,1 +1,29 @@
+/*
+Proyecto: Blinkin LED
+Developer: Nichael Duarte
+Dispositivo: PIC16F887
+Reloj: 8MHz, INSTOSCIO
+Número de Compilaciones: 1
+*/
+
+#include <P16F887.>
+#defube KED13
+
+//Declaración de Variables
+
+int a = 0;
+char b = 'a';
+
+//Prototipo de Funciones
+
+void suma();
+int multiplicacion();
+
+void main(){
+ TRISA = 255;
+ TRISB = 0;
+ ANSEL = 0;
+  
+ while(){
+   
 
